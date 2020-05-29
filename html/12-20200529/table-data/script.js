d680178366dc41e0e6d1b9b15584d83d818dce51
namnh06 <namnh.developer@gmail.com>
2020-05-29 19:22:24 +0700

    Updated ss12

---
+++ html/12-20200529/table-data/script.js
@@ -0,0 +1,48 @@
+var data = {
+    "users": [{
+            "id": 10,
+            "name": "Ethyl Medhurst",
+            "email": "noah.hand@example.org",
+            "password": "$2y$10$GLfHbVK./jG2WD8QQ0.ZDOqqeJe7rz.hl827sKoHOhd9LLoXfyHHG",
+            "created_at": "2019-03-26 04:55:04",
+            "updated_at": "2019-03-26 04:55:04"
+        },
+        {
+            "id": 9,
+            "name": "Eula Lebsack PhD",
+            "email": "schroeder.sylvan@example.org",
+            "password": "$2y$10$.vzCIUVX34GibJ3.ObFqYu5Bd.Ig8PqF3adoLHza78eq510mfhy9G",
+            "created_at": "2019-03-26 04:55:03",
+            "updated_at": "2019-03-26 04:55:03"
+        },
+        {
+            "id": 8,
+            "name": "Mrs. Kathlyn Parisian DDS",
+            "email": "ggrimes@example.net",
+            "password": "$2y$10$N.DpYW9Sd0c8p3rSVjO0MeZ5e7idfPeKPZyOO77m8vNSEOnMws5O2",
+            "created_at": "2019-03-26 04:55:03",
+            "updated_at": "2019-03-26 04:55:03"
+        }
+    ]
+};
+
+function hienThiNoiDung() {
+    var tableBodyElement = document.getElementById("tableBody");
+    for (var i = 0; i < data.users.length; i++) {
+        var tableRow = document.createElement("tr");
+
+        var tableData = document.createElement("td");
+        tableData.innerHTML = i + 1;
+        tableRow.appendChild(tableData);
+
+        var tableData = document.createElement("td");
+        tableData.innerHTML = data.users[i]["name"];
+        tableRow.appendChild(tableData);
+
+        var tableData = document.createElement("td");
+        tableData.innerHTML = data.users[i].email;
+        tableRow.appendChild(tableData);
+
+        tableBodyElement.appendChild(tableRow);
+    }
+}

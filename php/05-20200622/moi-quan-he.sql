b09956f362aea5a5bd2049872c80d9da71047ead
namnh06 <namnh.developer@gmail.com>
2020-06-22 21:17:50 +0700

    Finish php05 moi quan he nhieu nhieu

--- php/05-20200622/moi-quan-he.sql
+++ php/05-20200622/moi-quan-he.sql
@@ -54,3 +54,35 @@ FROM aptech_php_23_05.users AS a
 JOIN aptech_php_23_05.vehicles AS b
 ON a.id = b.user_id
 WHERE a.name = 'Nu';
+
+-- MANY TO MANY
+CREATE TABLE aptech_php_23_05.subjects (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE
+);
+
+INSERT INTO aptech_php_23_05.subjects (name)
+VALUES ('PHP'),('JS'),('HTML'),('CSS');
+
+CREATE TABLE aptech_php_23_05.subject_user(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    subject_id INT,
+    user_id INT,
+    FOREIGN KEY (subject_id) REFERENCES subjects(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO aptech_php_23_05.subject_user (subject_id,user_id)
+VALUES (1,2),(1,3),(2,1),(2,3),(3,3);
+
+-- QUERY LAY TAT CA MON HOC CUA HOC VIEN CO ID = 3 VA TEN CUA HO
+SELECT b.name as user_name, 
+c.name AS subject_name 
+FROM aptech_php_23_05.subject_user AS a
+JOIN aptech_php_23_05.users AS b
+ON a.user_id = b.id
+JOIN aptech_php_23_05.subjects AS c
+ON a.subject_id = c.id
+WHERE a.subject_id = 1;
+
+SELECT * FROM aptech_php_23_05.users;

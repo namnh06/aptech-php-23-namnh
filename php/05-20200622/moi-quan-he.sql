87d6421d6fcb284e77040b0cdfad4c4395adc7a7
namnh06 <namnh.developer@gmail.com>
2020-06-22 20:48:04 +0700

    Update php05 moi quan he mot nhieu

--- php/05-20200622/moi-quan-he.sql
+++ php/05-20200622/moi-quan-he.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE aptech_php_23_05;
 
+-- ONE TO ONE RELATIONSHIP
+
 CREATE TABLE aptech_php_23_05.users (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) UNIQUE
@@ -32,3 +34,23 @@ JOIN
 aptech_php_23_05.passports AS b
 ON a.id = b.user_id
 WHERE b.serial = 'b';
+
+
+-- ONE TO MANY RELATIONSHIP
+CREATE TABLE aptech_php_23_05.vehicles (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    bien_so VARCHAR(3) UNIQUE,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO aptech_php_23_05.vehicles (bien_so, user_id)
+VALUES ('3A2',2),('99B',2),('269',1);
+
+SELECT * FROM aptech_php_23_05.vehicles;
+
+SELECT a.name, b.bien_so 
+FROM aptech_php_23_05.users AS a
+JOIN aptech_php_23_05.vehicles AS b
+ON a.id = b.user_id
+WHERE a.name = 'Nu';
